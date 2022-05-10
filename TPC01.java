ecfed9ec89ad6f67dc1aaaf364169f8fc6a684ef
GitOfJY <104612045+GitOfJY@users.noreply.github.com>
2022-05-10 23:13:34 +0900

    Add files via upload

---
+++ TPC01.java
@@ -0,0 +1,20 @@
+
+public class TPC01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Java");
+
+	}
+
+}
+
+
+
+/*
+bin > exe 파일
+src > source 파일 
+
+java 프로그램은 os에 독립적으로 실행할 수 있다. > JVM이라는 가상머신이 구동하기 때문이다.
+1차 컴파일(byte code) > JVM 운영체제에 맞게 컴파일 
+*/
